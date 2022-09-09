02ccda9cf052e17532171594c1bbd1ec8b54a5c1
Akash Moradiya <akashmoradiya3444@gmail.com>
2022-09-09 21:37:10 +0530

    Make navbar responsive

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
 import { FiMenu } from "react-icons/fi";
+import { HiOutlineX } from "react-icons/hi";
 
 const navbarLinks = [
   {
@@ -18,6 +19,12 @@ const navbarLinks = [
 ];
 
 const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => setIsMenuOpen((prevIsMenuOpen) => !prevIsMenuOpen);
+
+  const MenuIcon = isMenuOpen ? HiOutlineX : FiMenu;
+
   return (
     <nav className="h-20 fixed top-0 inset-x-0 bg-slate-800/60 backdrop-filter backdrop-blur-sm z-20">
       <div className="max-w-screen-lg mx-auto h-full flex items-center justify-between px-8">
@@ -25,7 +32,7 @@ const Navbar = () => {
           Akash
         </a>
 
-        <ul className="flex items-center gap-y-3 gap-x-14">
+        <ul className="hidden md:flex items-center gap-y-3 gap-x-14">
           {navbarLinks.map((link, index) => (
             <li
               key={index}
@@ -35,9 +42,27 @@ const Navbar = () => {
             </li>
           ))}
         </ul>
+        <ul
+          className={`${
+            isMenuOpen
+              ? "flex flex-col items-center gap-y-1 gap-x-14"
+              : "hidden"
+          } md:hidden absolute top-full inset-x-0 h-[calc(100vh-5rem)] bg-slate-800 backdrop-filter backdrop-blur-sm z-20`}
+        >
+          {navbarLinks.map((link, index) => (
+            <li
+              key={index}
+              className="w-full text-center text-white font-inter font-medium transition-colors duration-200 hover:bg-slate-700 py-4"
+            >
+              <Link href={link.href}>
+                <a className="block w-full">{link.text}</a>
+              </Link>
+            </li>
+          ))}
+        </ul>
 
-        <button className="md:hidden">
-          <FiMenu size={25} className="text-white" />
+        <button className="md:hidden" onClick={toggleMenu}>
+          <MenuIcon size={25} className="text-white" />
         </button>
       </div>
     </nav>
