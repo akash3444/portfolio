96c2885e42ccf9f36fc7c818ac32494b4dc1ee59
Akash Moradiya <akashmoradiya3444@gmail.com>
2022-09-11 09:50:02 +0530

    Add google-site-verification meta

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,12 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html>
-      <Head></Head>
+      <Head>
+        <meta
+          name="google-site-verification"
+          content="Y3cxu2H6adLPRZby5rb9of9fxPKMuPTmC0i_p4So-n4"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
